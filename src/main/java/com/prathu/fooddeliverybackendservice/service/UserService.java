433089c22d6a0ee4f849713655ee63a3af957863
Pavan3030-pr <pavan20053010@gmail.com>
2026-06-14 13:14:30 +0530

    Day 7: BCrypt password encryption + secure login implemented

--- src/main/java/com/prathu/fooddeliverybackendservice/service/UserService.java
+++ src/main/java/com/prathu/fooddeliverybackendservice/service/UserService.java
@@ -4,6 +4,7 @@ import com.prathu.fooddeliverybackendservice.model.User;
 import com.prathu.fooddeliverybackendservice.repository.UserRepository;
 import com.prathu.fooddeliverybackendservice.utils.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,8 +16,15 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public String registerUser(User user) {
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
         userRepository.save(user);
+
         return "User saved in DB: " + user.getName();
     }
 
@@ -28,7 +36,7 @@ public class UserService {
             return "User not found";
         }
 
-        if (user.getPassword().equals(password)) {
+        if (passwordEncoder.matches(password, user.getPassword())) {
             return jwtUtil.generateToken(email);
         }
 
